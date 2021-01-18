496971b28b878fa74855142030adc408ac21bec4
Merlau Catalin <merlau.catalin@gmail.com>
2021-01-18 06:11:50 +0200

    update 1

--- Proiect Cinema/Bilet.h
+++ Proiect Cinema/Bilet.h
@@ -1,52 +1,66 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include "Film.h"
-#include "Sala.h"
 using namespace std;
 #define _CRT_SECURE_NO_WARNINGS
 
 class Bilet
 {
 	const int idBilet;
-	static int numarBilet;
-	Film numeFilm;
-	Film durata;
-	double pretBilet;
-	Sala numeSala;
-	Sala randDinSala;
-	Sala locDinSala; //=>de aici trebuie creata o functie care sa imi adune
-	//numarul de locuri ocupate din acea sala, gen locuriOcupate()
-
+	static string numeCinema;
+	//Film numeFilm;
+	//Film durata;
+	char* numeFilm;
+	int idSala;
+	int nrLoc;
+	int nrRand;
+	int nrBilete;
+	double* pretBilet;
+	//Sala numeSala;
+	//Sala randDinSala;
+	//Sala locDinSala;
 public:
-	//Bilet() : idBilet(numarBilet)
-	//{
-	//	pretBilet = 0;
-	//}
-
+	//constructor implicit
+	Bilet();
 	//constructor cu parametri
-
+	Bilet(char*, int, int, int, int, double*);
 	//constructorul de copiere
-
+	Bilet(const Bilet& b);
 	//destructorul
-
+	~Bilet();
 	//operatorul =
-
+	Bilet& operator=(const Bilet& b);
 	//operator []
-
+	double& operator[](int);
 	//un operator matematic
-
+	Bilet operator+(int);
 	//operatorul ++ sau -- (cele 2 forme)
-
+	Bilet operator++();
+	Bilet operator++(int);
 	//operator cast (catre orice tip) explicit sau implicit
-
+	explicit operator int();
 	//operator !
-
+	bool operator!();
 	//operator conditional (<.>,=<,>=)
-
+	bool operator<(Bilet b1);
 	//operatorul pentru testarea egalitatii dintre 2 obiecte ==
-
+	bool operator==(Bilet b1);
 	//friend istream >> si ostream <<
+	friend ostream& operator<<(ostream& out, Bilet);
+	friend istream& operator>>(istream& in, Bilet&);
+
+	char* getNumeFilm();
+	void setNumeFilm(const char*);
+
+	int getNrBilete();
+	void setNrBilete(int);
+
+	double* getPretBilet();
+	void setPretBilet(double* pretBilet, int nrBilet);
 };
 
-int Bilet::numarBilet = 0;
+Bilet operator+(int, Bilet);
+bool operator==(const Bilet& sb1, const Bilet& b2);
+bool operator<(const Bilet& b1, const Bilet& b2);
+ostream& operator<<(ostream& out, Bilet b);
+istream& operator>>(istream& in, Bilet& b);
